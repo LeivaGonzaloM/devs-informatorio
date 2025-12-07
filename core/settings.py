d1fa8aa7ac96a562e631ac5847f8e9920b52f4c6
Leiva Gonzalo <gonzalo.manuel.leiva@gmail.com>
2025-12-07 05:30:05 -0300

    Agregar STATIC_ROOT para deploy en PythonAnywhere

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,6 +125,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+# Define STATIC_ROOT como un directorio llamado 'staticfiles_prod' en la raíz del proyecto
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
